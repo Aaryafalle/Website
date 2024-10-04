17af70306c0309819cb28f5308a1b08e835fe660
Eshan05 <nahareshan05@gmail.com>
2024-10-04 14:48:50 +0530

    Reimplement modal to share and copy link

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
-  const parentLink = el.parentNode.closest("a");
-  const link = parentLink.getAttribute("href");
-  console.log(link);
-});
-
+// TODO: Make it so that I can get the link in the messages
 document.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
     document.querySelector(".modal")?.remove();
@@ -13,36 +8,48 @@ document.addEventListener("keydown", function (event) {
 document.querySelectorAll("i.fa-ellipsis-vertical").forEach((el) => {
   el.addEventListener("click", (e) => {
     e.preventDefault();
+
+    const parentLink = el.closest("a");
+    const link = parentLink.getAttribute("href");
+
     const modal = document.createElement("div");
     modal.classList.add(
-      "fixed",
-      "inset-0",
-      "bg-black",
-      "bg-opacity-50",
-      "flex",
-      "items-center",
-      "justify-center",
-      "modal",
-      "z-20",
-      "h-[100%]"
+      "fixed", "inset-0", "bg-black", "bg-opacity-50",
+      "flex", "items-center", "justify-center", "modal", "z-20", "h-[100%]"
     );
+
     const menu = document.createElement("ul");
     menu.classList.add("bg-[#2226]", "rounded", "p-2");
     menu.innerHTML = `
         <h2> Share The Link! </h2>
-        <li><i class="fa-brands fa-x"></i><a href="https://x.com/intent/tweet?text=Checkout " class="modal-share">Share via X</a></li>
-        <li><i class="fa-brands fa-snapchat" class="text-[#fdfd69]"></i><a href="snapchat://add" class="modal-share">Share via Snapchat</a></li>
-        <li><i class="fa-brands fa-linkedin"></i><a href="linkedin://sharing/share-offsite" class="modal-share">Share via LinkedIn</a></li>
-        <li><i class="fa-solid fa-envelope"></i><a href="mailto:?subject=Checkout This Link!" class="modal-share">Share via Email</a></li>
-        <li><i class="fa-brands fa-whatsapp"></i><a href="https://wa.me/?text=Check This Link!" class="modal-share">Share via WhatsApp</a></li>
-        <li><i class="fa-solid fa-share"></i><button class="modal-share" onclick="if (navigator.clipboard) { navigator.clipboard.writeText(window.location.href); this.innerText = 'Copied!'; setTimeout(() => { this.innerText = 'Copy Link'; }, 2000); } else { alert('Your browser does not have support for the Clipboard API'); }">Copy Link</button></li>
+        <li><i class="fa-brands fa-x-twitter text-neutral-600"></i><a href="https://x.com/intent/tweet?text=Checkout ${encodeURIComponent(link)}" class="modal-share">Share via X</a></li>
+        <li><i class="fa-brands fa-snapchat text-yellow-400"></i><a href="snapchat://add" class="modal-share">Share via Snapchat</a></li>
+        <li><i class="fa-brands fa-linkedin text-blue-500"></i><a href="https://www.linkedin.com/shareArticle?url=${encodeURIComponent(link)}" class="modal-share">Share via LinkedIn</a></li>
+        <li><i class="fa-regular fa-envelope text-red-500"></i><a href="mailto:?subject=Checkout This Link!&body=${encodeURIComponent(link)}" class="modal-share">Share via Email</a></li>
+        <li><i class="fa-brands fa-whatsapp text-green-500"></i><a href="https://wa.me/?text=${encodeURIComponent(link)}" class="modal-share">Share via WhatsApp</a></li>
+        <li><i class="fa-solid fa-share ml-px md:ml-0"></i><button class="modal-share whitespace-pre truncate items-center flex justify-evenly gap-x-2" id="copy-link-btn">Copy Link <span class="text-[.5em] p-1 md:p-1.5 bg-gray-600/20 rounded overflow-x-scroll text-ellipsis rounded whitespace-pre inline-block w-[10rem] md:w-[15rem]">${encodeURIComponent(link)}</span></button></li>
       `;
+
     modal.appendChild(menu);
     document.body.appendChild(modal);
+
     modal.addEventListener("click", (e) => {
       if (e.target === modal) {
         modal.remove();
       }
     });
+
+    // Copy link functionality
+    document.getElementById("copy-link-btn").onclick = () => {
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(link).then(() => {
+          const button = document.getElementById("copy-link-btn");
+          button.innerText = 'Copied!';
+          setTimeout(() => { button.innerText = 'Copy Link'; }, 2000);
+        });
+      } else {
+        alert('Your browser does not support the Clipboard API');
+      }
+    };
   });
 });
